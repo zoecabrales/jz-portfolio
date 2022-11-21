ff2c0757d4b74c10daeafed700c47ff0304decdd
zoecabrales <zoe@old.st>
2022-11-21 21:52:58 +0800

    added scrollToTop functionality for routing

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./hooks/scrollToTop";
 
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 
 ReactDOM.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>,
   document.getElementById("root")
