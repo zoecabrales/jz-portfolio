ff2c990e4d3c870b21428dc2d072aaa19ce160cf
zoecabrales <zoe@old.st>
2022-12-21 06:11:18 +0800

    added exp section to Home Page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,7 @@ import Footer from "../components/Footer/Footer";
 import Work from "../components/data/Work";
 import About from "../components/About/AboutContent";
 import CertCard from "../components/Cards/CertCard/CertCard";
+import Exp from "../components/data/Exp";
 
 const Home = ({ heading, text }) => {
   return (
@@ -12,6 +13,7 @@ const Home = ({ heading, text }) => {
       <Navbar />
       <HeroImg />
       <Work />
+      <Exp />
       <About />
       <CertCard />
       <Footer />
